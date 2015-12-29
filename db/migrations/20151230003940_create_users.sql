e2e24f9c64e12b89e2dac190ae12caea19da14e9
Yuki Iwanaga <yuki@creasty.com>
2015-12-30 01:34:58 +0900

    Tweak

--- db/migrations/20151230003940_create_users.sql
+++ db/migrations/20151230003940_create_users.sql
@@ -1,11 +1,23 @@
 -- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
+-- +goose StatementBegin
 create table users (
-    id     serial primary key not null,
-    github varchar(39),
+    id     serial primary key,
+    github varchar(39) unique not null,
     name   varchar(64)
 );
 
+begin;
+  update users
+  set
+    github = 'creasty',
+    name = 'Yuki Iwanaga'
+  where id = 1;
+
+  insert into users (id, github, name)
+  select 1, 'creasty', 'Yuki Iwanaga'
+    where not exists (select 1 from users where id = 1);
+commit;
+-- +goose StatementEnd
+
 -- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
 drop table users;
