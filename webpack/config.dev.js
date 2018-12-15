bde313cb33f35863df518266669a6f37c9efad41
Yuki Iwanaga <yuki@creasty.com>
2018-12-16 00:44:42 +0900

    Reconfigure webpack and build

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -1,61 +1,29 @@
-const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const DEV_SERVER = 'http://localhost:9100';
 
 module.exports = {
-  devtool: 'eval',
-  context: path.resolve(__dirname, '../src'),
-  entry: [
-    'webpack-dev-server/client?' + DEV_SERVER,
-    './main.js',
-  ],
-  resolve: {
-    extensions: ['', '.js', 'index.js'],
+  devtool: 'inline-source-map',
+
+  devServer: {
+    host,
+    port,
+    publicPath,
+    inline: true,
+    lazy: false,
+    hot: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 100,
+    },
+    setup(app) {
+      app.get('/ping', (_req, res) => {
+        res.send('pong');
+      });
+    },
   },
-  output: {
-    path: path.resolve(__dirname, '../public/assets'),
-    filename: '[name].js',
-    publicPath: DEV_SERVER + '/assets/',
-  },
+
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true,
-      nodeEnv: process.env.NODE_ENV,
-    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({ minimize: false, debug: true }),
   ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['babel'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(css|scss)$/,
-        loaders: [
-          'style',
-          'css',
-          'sass',
-          'import-glob-loader',
-          'postcss',
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|mp4|eot|woff2?|ttf|svg)$/,
-        loader: 'file?name=[name].[ext]',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  postcss: function() {
-    return [autoprefixer];
-  },
 };
